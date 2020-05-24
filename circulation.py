efe969dff376ddbae7f41a12ff153bcb1c3ccbca
Sirotkin <aasirotkin@mail.ru>
2020-05-24 17:43:14 +0300

    Ogive

--- circulation.py
+++ circulation.py
@@ -74,6 +74,51 @@ class Triangle(Figure):
         super().__init__('Triangle', coord_x, coord_y)
 
 
+class Ogive(Figure):
+    @staticmethod
+    def tangent(xc: float, yc: float, r: float, xt: float, yt: float):
+        h1 = abs(yc - yt)
+        h2 = abs(xc - xt)
+        h1h2 = np.sqrt(h1 * h1 + h2 * h2)
+        a = np.arcsin(r / h1h2)
+        b = np.arcsin(h1 / h1h2)
+        kt = np.tan(a + b)
+        kc = -1.0 / kt
+        bt = yt - kt*xt
+        bc = yc - kc*xc
+        x0 = (bc - bt) / (kt - kc)
+        y0 = kt * x0 + bt
+        h4 = abs(y0 - yc)
+        gamma = 2.0 * np.arccos(h4 / r)
+        return x0, y0, kt, bt, gamma
+
+    def __init__(self, base_r: float, nose_r: float, length: float,
+                 num_points: int = 20):
+        assert length > base_r > nose_r > 0
+        # X, Y coordinates of ogive (it look's like warhead)
+        x0, y0, kt, bt, gamma = \
+            self.tangent(0.0, length - nose_r, nose_r, -base_r, 0.0)
+        gamma1 = 0.5 * (np.pi - gamma)
+        gamma2 = gamma1 + gamma
+
+        x1 = np.linspace(-base_r, x0, num_points)
+        y1 = kt * x1 + bt
+
+        tetta = np.linspace(gamma2, gamma1, num_points)
+        x2 = nose_r * np.cos(tetta)
+        y2 = nose_r * np.sin(tetta) + length - nose_r
+
+        x3 = -1.0 * x1[::-1]
+        y3 = y1[::-1]
+
+        x4 = np.linspace(base_r, -base_r, num_points)
+        y4 = np.array([0]*num_points)
+
+        coord_x = np.concatenate([x1, x2, x3, x4])
+        coord_y = np.concatenate([y1, y2, y3, y4])
+        super().__init__('Ogive', coord_x, coord_y)
+
+
 class Circulation:
     def __init__(self, figure: Figure):
         self.figure = figure
@@ -87,8 +132,8 @@ class Circulation:
         max_x_value = int(x_coord.max() + 1)
         min_y_value = int(y_coord.min() - 1)
         max_y_value = int(y_coord.max() + 1)
-        points_x = max_x_value - min_x_value + 1
-        points_y = max_y_value - min_y_value + 1
+        points_x = max(5, max_x_value - min_x_value + 1)
+        points_y = max(5, max_y_value - min_y_value + 1)
 
         # X, Y grid points
         cls.x = np.linspace(min_x_value, max_x_value, points_x)
@@ -119,7 +164,8 @@ class Circulation:
         plt.plot(self.xx, self.yy, 'k.')
         plt.quiver(self.xx, self.yy, self.rvx, self.rvy, color='r')
         plt.plot(self.figure.x_coord, self.figure.y_coord, 'b.')
-        plt.quiver(self.figure.x_coord, self.figure.y_coord, self.vx, self.vy, color='b')
+        plt.quiver(self.figure.x_coord, self.figure.y_coord,
+                   self.vx, self.vy, color='b')
         plt.title('{} Circulation = {:.3f}'.format(self.figure.name, self.gamma))
         plt.xlim([self.xx.min()-1, self.xx.max()+1])
         plt.ylim([self.yy.min()-1, self.yy.max()+1])
@@ -128,16 +174,19 @@ class Circulation:
 
 
 if __name__ == '__main__':
-    fig = Ellipse(15, 15, 10, 10)
-    fig = Square(15, 10, -5, 10)
-    fig = Triangle((10, 10), (20, 20), (15, -5))
+    # fig = Ellipse(15, 15, 10, 10)
+    # circulation = Circulation(fig)
+    # circulation.plot()
+
+    # fig = Square(15, 10, -5, 10)
+    # circulation = Circulation(fig)
+    # circulation.plot()
+
+    # fig = Triangle((10, 10), (20, 20), (15, -5))
+    # circulation = Circulation(fig)
+    # circulation.plot()
+
+    fig = Ogive(3, 1, 9)
     circulation = Circulation(fig)
     circulation.plot()
-    # ellipse = EllipseCirculation(1*np.pi, 1*np.pi, 0, 0)
-    # ellipse.plot()
-    # square = SquareCirculation(15, 15, 0, 0)
-    # square.plot()
-    # triangle = TriangleCirculation((10, 10), (20, 20), (15, -5))
-    # triangle.plot()
-    pass
 
